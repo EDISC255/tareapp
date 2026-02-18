1b4c89bed3796d0981ee09b637d13f675afcf205
Eduardo Cabrera Montero <isc.eduardo.came@gmail.com>
2026-02-18 17:07:41 -0600

    se creo la clase conexion

--- tareas/src/control/Conexion.java
+++ tareas/src/control/Conexion.java
@@ -3,10 +3,75 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package control;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author eduardo
  */
 public class Conexion {
-    
+
+    private Connection con;
+    private PreparedStatement pst;
+    private Statement st;
+    private ResultSet rs;
+
+    public Conexion(String rutaBD) {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            File bd = new File(rutaBD);
+            con = DriverManager.getConnection("jdbc:sqlite://" + bd.getAbsolutePath());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Conexion(String URL, String usuario, String contrasena) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(URL, usuario, contrasena);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public PreparedStatement getPst() {
+        return pst;
+    }
+
+    public void setPst(PreparedStatement pst) {
+        this.pst = pst;
+    }
+
+    public void setRs(ResultSet rs) {
+        this.rs = rs;
+    }
+
+    public ResultSet getRs() {
+        return rs;
+    }
+
+    public void setSt(Statement st) {
+        this.st = st;
+    }
+
+    public Statement getSt() {
+        return st;
+    }
+
 }
